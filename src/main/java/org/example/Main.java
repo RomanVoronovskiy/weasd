d565f42923f09abab61ea6afdefbf07b9d86ba47
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2025-08-01 12:42:15 +0300

    five

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("сделал 1");
         System.out.println("сделал 2");
         System.out.println("сделал 3");
+        System.out.println("сделал 5");
     }
 }
