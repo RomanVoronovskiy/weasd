19b25f7a12b095168d046465469d739cf08405f1
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2025-08-01 12:27:19 +0300

    second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("сделал 1");
+        System.out.println("сделал 2");
+        System.out.println("сделал 3");
     }
 }
